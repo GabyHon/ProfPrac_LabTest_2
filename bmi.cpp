d3bfc0a27422ac2afdabbdb6ebaf7641c4ab11d0
Gabriella Hon <gabriella.hon@students.wits.ac.za>
2025-04-29 19:17:32 +0200

    Correcting Program
    Updated the conversion functions that were incorrectly converting. Correctetd the categories conditions. Checked if three arguments were supplied.

--- bmi.cpp
+++ bmi.cpp
@@ -1,4 +1,7 @@
 // KJN - it is good practice to insert comment about intended use, context, contributors, etc
+// This file contains the functions to convert weights and heights,
+// process data from an input file and output to a text file with the result of the BMI category
+// Contributors: KJN, GH
 
 #include <iostream>
 #include <fstream>
@@ -8,7 +11,7 @@ using namespace std;
 
 int ounces2pounds(int x)
 {
-    return(x*16);
+    return(x/16);
 }
 
 int stones2pounds(int x)
@@ -23,18 +26,21 @@ double weight2kg(int stones, int pounds, int ounces)
 
 double height2metres(int feet, int inches)
 {
-    return(feet/3.82);
+    //convert inches to feet and then convert feet to metres
+    double total_feet = feet + inches/12;
+    return total_feet/3.28;
 }
 
+// Categories for the BMI
 char categorise(double kg, double metre)
 {
-    double bmi = kg*kg/metre;
+    double bmi = kg/(metre*metre);
     char cat;
     if (bmi<19)
         cat='A';
-    else if (bmi<=26)
+    else if (bmi<25)
         cat='B';
-    else if (bmi<=30)
+    else if (bmi<30)
         cat='C';
     else
         cat='D';
@@ -64,9 +70,16 @@ void process_data(char* input_file, char* output_file)
     f_in.close();
     f_out.close();
 }
-        
+
 int main(int argc, char *argv[])
 {
+    // Checking if 3 arguments are supplied, otherwise output error message
+    if(artgc != 3)
+	{
+	   cerr << argv[0] << "<input_file> <output_file>" << endl;
+	   return 1;
+	}
     // KJN - Need to check that 3 arguments were supplied upon execution
     process_data(argv[1], argv[2]);
+    return 0;
 }
